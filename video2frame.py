38d27b922ba7819ba60fbb7a2ff56ade3386574b
Antor Ahmed <antor.mee@gmail.com>
2023-05-21 03:49:19 +0600

    update model

--- video2frame.py
+++ video2frame.py
@@ -1,18 +1,23 @@
 import cv2
+import time
 
-fileName = "r1"
-folderName = "freeze on"
+fileName = "FanOn1"
+folderName = "FanOn"
 outputPath = "output\\" + folderName + "\\"
   
 def FrameCapture(path):
     vidObj = cv2.VideoCapture(path)
     count = 1
     success = 1
+    prevTime = 0.0
     while success:
         success, image = vidObj.read()
-        cv2.imwrite(outputPath + "frame%d.jpg" % count, image)
-        print("frame%d" % count)
-        count += 1
+        if not success: return
+        if time.time() - prevTime > 0.1:
+            cv2.imwrite(outputPath + "%s%d.jpg" % (fileName, count), image)
+            print("frame%d" % count)
+            prevTime = time.time()
+            count += 1
   
 if __name__ == '__main__':
     FrameCapture("%s.mp4" % fileName)
